706514eb80c37b90aee611e5eb8aa2266d741fe4
earloc <earloc@live.de>
2020-05-01 01:04:11 +0200

    add pong-action

--- src/ApiBox.Net/AspNetCore/Rest/PingController.cs
+++ src/ApiBox.Net/AspNetCore/Rest/PingController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using ApiBox.PingPong;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +11,18 @@ namespace ApiBox.Net.AspNetCore.Rest
     public class PingController : ControllerBase
     {
         [HttpGet]
+        public async Task<ActionResult<IEnumerable<PingResponse>>> Pongs ([FromServices] IPingPong pingPong)
+        {
+            var query = await pingPong.GetAsync();
+            var result = query.ToArray();
+
+            return Ok(result.Select(_ => new PingResponse(_)));
+        }
+
+        [HttpPost]
         public async Task<ActionResult<PingResponse>> Ping (string message, [FromServices] IPingPong pingPong) {
             var pong = await pingPong.PingAsync(new Ping(message));
-            return new PingResponse(pong);
+            return Ok(new PingResponse(pong));
         }
     }
 }
